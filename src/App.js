8f5ba53536e56b50dd655c1e5f32281a0bd3a59b
truong hung <truonghungvatvo@gmail.com>
2021-04-09 00:07:53 +0700

    center

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello world</p>
-    </div>
+    <p>Hello world</p>
   );
 }
 
