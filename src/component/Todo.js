522a293150a9e62f5522063df1ad5b230b834aa4
EC2 Default User <ec2-user@ip-172-31-7-179.ec2.internal>
2021-04-11 11:51:27 +0000

    Add_save_localstorage

--- src/component/Todo.js
+++ src/component/Todo.js
@@ -1,131 +1,124 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../assets/css/Todo.css';
 import TodoItem from './TodoItem';
 import {getId} from '../lib/util';
 import DateTimePicker from 'react-datetime-picker';
 
+import useStorage from '../hooks/storage';
+
 function CreateTask({ addTask }) {
         const [value, setValue] = useState("");
         const [description, setDescription] = useState("");
-        const [dateTime, SetDateTime] = useState("");
+        const [dateTime, SetDateTime] = useState(new Date());
 
         const handleSubmit = e => {
             e.preventDefault();
             if (!value) return;
-
             var date = dateTime.toLocaleString();
-            addTask(value, description, date);
             
+            addTask(value, description, date);
+            console.log(value, description, date);
             setValue("");
             setDescription("");
             SetDateTime("");
         }
 
         return (
-            <>
-                <form onSubmit={handleSubmit}>
-                    <input
-                        type="text"
-                        className="input"
-                        value={value}
-                        placeholder="Add a new task"
-                        onChange={e => setValue(e.target.value)}
-                        required
-                    />
-                    <input
-                        type="text"
-                        className="input"
-                        value={description}
-                        placeholder="Add description"
-                        onChange={e => setDescription(e.target.value)}
-                    />
-                    <DateTimePicker
-                        className="dateTime"
-                        onChange={SetDateTime}
-                        value={dateTime}
-                    />
-                    <input type="submit" value="Add" />
-                </form>
-            </>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    className="input"
+                    value={value}
+                    placeholder="Add a new task"
+                    onChange={e => setValue(e.target.value)}
+                />
+                <input
+                    type="text"
+                    className="input"
+                    value={description}
+                    placeholder="Add description"
+                    onChange={e => setDescription(e.target.value)}
+                />
+                <DateTimePicker
+                    onChange={SetDateTime}
+                    value={dateTime}
+                />
+                <input type="submit" value="Submit" />
+            </form>
         );
     }
 
 
 function Todo() {
         const [tasksRemaining, setTasksRemaining] = useState(0);
-        const [tasks, setTasks] = useState([
-            {
-                id: getId(),
-                title: "Grab some Pizza",
-                description: "An com voi ma",
-                dateTime: "11/1/11",
-                completed: true
-            },
-            {
-                id: getId(),
-                title: "Do your workout",
-                description: "An com voi ma",
-                dateTime: "11/1/11",
-                completed: true
-            },
-            {
-                id: getId(),
-                title: "Hangout with friends",
-                description: "An com voi ma",
-                dateTime: "11/1/11",
-                completed: false
-            }
-        ]);
+        // const [tasks, setTasks] = useState([
+        //     {
+        //         id: getId(),
+        //         title: "Grab some Pizza",
+        //         description: "An com voi ma",
+        //         dateTime: "11/1/11",
+        //         completed: true
+        //     },
+        //     {
+        //         id: getId(),
+        //         title: "Do your workout",
+        //         description: "An com voi ma",
+        //         dateTime: "11/1/11",
+        //         completed: true
+        //     },
+        //     {
+        //         id: getId(),
+        //         title: "Hangout with friends",
+        //         description: "An com voi ma",
+        //         dateTime: "11/1/11",
+        //         completed: false
+        //     }
+        // ]);
+        
+          const [tasks, putItems, clearItems] = useStorage();
         useEffect(() => {
           setTasksRemaining(tasks.filter(task => !task.completed).length)
         }, [tasks]);
 
-        const addTask = useCallback((title, description, dateTime) => {
+        const addTask = (title, description, dateTime) => {
             var id = getId();
             const newTasks = [...tasks, {id, title, description, dateTime, completed: false }];
-            setTasks(newTasks);
-        }, [tasks]);
-
-        const completeTask = (idItem) => {
-            const newTask = tasks.map(task => {
-                if (task.id === idItem) {
-                    task.completed = !task.completed;
-                }
-                return task;
-              });
-              setTasks(newTask);
+            putItems(newTasks);
         };
 
-        const removeTask = (idItem) => {
+        const completeTask = (index) => {
             const newTasks = [...tasks];
-            setTasks(newTasks.filter(({ id }) => id !== idItem));
+            newTasks[index].completed = true;
+            putItems(newTasks);
         };
 
-        const sortByTime = useCallback(() => {
-            const tasksSorted = [...tasks].sort((a, b) => {
-                return a.dateTime < b.dateTime ? -1 : 1
-            });
-            setTasks(tasksSorted);
-            console.log(tasksSorted);
-        }, [tasks]);
+        const removeTask = (index) => {
+            const newTasks = [...tasks];
+            newTasks.splice(index, 1);
+            putItems(newTasks);
+        };
 
         return (
             <div className="todo-container">
+                <div className="header">Pending tasks ({tasksRemaining})</div>
+                <div className="tasks">
+                    {tasks.map((task, index) => (
+                        <TodoItem
+                        task={task}
+                        index={index}
+                        completeTask={completeTask}
+                        removeTask={removeTask}
+                        key={index}
+                        />
+                    ))}
+                </div>
                 <div className="create-task" >
                     <CreateTask addTask={addTask} />
                 </div>
-                <div className="header">Pending tasks ({tasksRemaining})</div>
-                <button onClick={() => sortByTime()}>Sort</button>
-                <div className="tasks">
-                    {tasks.map((task) => (
-                        <TodoItem
-                        task={task}
-                        id={task.id}
-                        completeTask={completeTask}
-                        removeTask={removeTask}
-                        key={task.id}
-                        />
-                    ))}
+                <div className="panel-block">
+                    <button className="button is-light is-fullwidth" onClick={clearItems}>
+                        Remove all Tasks
+                    </button>
                 </div>
             </div>
         );    }
--- src/component/TodoItem.js
+++ src/component/TodoItem.js
@@ -1,18 +1,15 @@
 import React from 'react'
-import '../assets/css/TodoItem.css';
+import './Todo.js';
 
-export default function TodoItem({ task, id, completeTask, removeTask }) {
+export default function TodoItem({ task, index, completeTask, removeTask }) {
     return (
-        <div className="task">
-            <div className="infor" style={{ textDecoration: task.completed ? "line-through" : "" }}>
-                <p className="titleItem">{task.title}</p>
-                <p className="description">{task.description}</p>
-                <p>{task.dateTime}</p>
-            </div>
-            <div className="control">
-                <button className="controlBtn" onClick={() => removeTask(id)}>Delete</button>
-                <button className="controlBtn" onClick={() => completeTask(id)}>Complete</button>
-            </div>
+        <div className="task" style={{ textDecoration: task.completed ? "line-through" : "" }}>
+            <p>{task.title}</p>
+            <p>{task.description}</p>
+            <p>{task.dateTime}</p>
+            
+            <button style={{ background: "red" }} onClick={() => removeTask(index)}>x</button>
+            <button onClick={() => completeTask(index)}>Complete</button>
         </div>
     )
 }
