9c9cad151f5536968fb18b10789a8b5193eb04d7
phu-bui <hunter17599@gmail.com>
2021-05-19 23:30:01 +0700

    Use firebase

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,6 +5,6 @@ var firebaseConfig = {
     projectId: "fir-sample-669f4",
     storageBucket: "fir-sample-669f4.appspot.com",
     messagingSenderId: "380097144719",
-    appId: "1:380097144719:web:b047f9d6f41ca12d53cdfa"
+    appId: "1:380097144719:web:a5b8f01b18f6bffb53cdfa"
   };
 firebase.initializeApp(firebaseConfig);
